afdd53ca118b0ce23fb30370f0802238ee1c759b
ChrisM345 <chrism.12277@gmail.com>
2024-09-28 18:11:18 -0500

    Added member signup based on secret password

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,12 @@
 const auth = require("../auth");
+const { body, validationResult } = require("express-validator");
+const { setMemberStatus } = require("../db/queries");
 
-const get = (req, res) => {
-  res.render("indexView", { title: "Members Only" });
+const validateSecret = [body("secret").trim().equals("verysecret").withMessage("Incorrect Secret")];
+
+const get = async (req, res) => {
+  console.log(await req.user);
+  res.render("indexView", { title: "Members Only", user: await req.user });
 };
 const getSignup = (req, res) => {
   res.render("signupView", { title: "Signup Form", data: {} });
@@ -24,9 +29,44 @@ const userLogin = (req, res, next) => {
   })(req, res, next);
 };
 
+const logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+};
+
+const getSecretSignup = (req, res, next) => {
+  res.render("secretSignupView", { title: "Secret Signup" });
+};
+
+const postSecretSignup = [
+  validateSecret,
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("secretSignupView", {
+        title: "Secret Signup",
+        errors: errors.array(),
+      });
+    }
+    console.log("correct secret");
+    console.log(req.session.passport.user);
+    console.log(req.session.user);
+    console.log("correct secret");
+    setMemberStatus(req.session.passport.user);
+    res.redirect("/");
+  },
+];
+
 module.exports = {
   get,
   getSignup,
   getLogin,
   userLogin,
+  logout,
+  getSecretSignup,
+  postSecretSignup,
 };
